11687305dbca97892b1e8d8dabb9995c1d9ed0d2
Quỳnh <quynhtruongmanh@vccorp.vn>
2017-01-23 17:29:26 +0700

    sửa DB

--- database/db_cms.sql
+++ database/db_cms.sql
@@ -4,22 +4,22 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50505
 Source Host           : localhost:3306
-Source Database       : laostyle
+Source Database       : db_cms
 
 Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-01-23 10:39:23
+Date: 2017-01-23 17:29:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for group_user
+-- Table structure for cms_group_user
 -- ----------------------------
-DROP TABLE IF EXISTS `group_user`;
-CREATE TABLE `group_user` (
+DROP TABLE IF EXISTS `cms_group_user`;
+CREATE TABLE `cms_group_user` (
   `group_user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id nhom nguoi dung',
   `group_user_name` varchar(50) NOT NULL COMMENT 'Ten nhom nguoi dung',
   `group_user_status` int(1) NOT NULL DEFAULT '1' COMMENT '1 : hiá»‡n , 0 : áº©n',
@@ -28,31 +28,16 @@ CREATE TABLE `group_user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of group_user
+-- Records of cms_group_user
 -- ----------------------------
-INSERT INTO `group_user` VALUES ('1', 'Root', '1', '1');
-INSERT INTO `group_user` VALUES ('2', 'Quyền xem lượt Share', '1', '1');
+INSERT INTO `cms_group_user` VALUES ('1', 'Root', '1', '1');
+INSERT INTO `cms_group_user` VALUES ('2', 'Quyền xem lượt Share', '1', '1');
 
 -- ----------------------------
--- Table structure for group_user_permission
+-- Table structure for cms_permission
 -- ----------------------------
-DROP TABLE IF EXISTS `group_user_permission`;
-CREATE TABLE `group_user_permission` (
-  `group_user_id` int(11) NOT NULL COMMENT 'id nhÃ³m',
-  `permission_id` int(11) NOT NULL COMMENT 'id quyÃ¨n'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of group_user_permission
--- ----------------------------
-INSERT INTO `group_user_permission` VALUES ('1', '1');
-INSERT INTO `group_user_permission` VALUES ('2', '43');
-
--- ----------------------------
--- Table structure for permission
--- ----------------------------
-DROP TABLE IF EXISTS `permission`;
-CREATE TABLE `permission` (
+DROP TABLE IF EXISTS `cms_permission`;
+CREATE TABLE `cms_permission` (
   `permission_id` int(11) NOT NULL AUTO_INCREMENT,
   `permission_code` varchar(50) NOT NULL COMMENT 'MÃ£ quyá»n',
   `permission_name` varchar(50) NOT NULL COMMENT 'TÃªn quyá»n',
@@ -62,79 +47,83 @@ CREATE TABLE `permission` (
 ) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of permission
+-- Records of cms_permission
 -- ----------------------------
-INSERT INTO `permission` VALUES ('1', 'root', 'Root', '1', 'Root');
-INSERT INTO `permission` VALUES ('2', 'supper_admin', 'Admin', '1', 'Admin');
-INSERT INTO `permission` VALUES ('3', 'user_create', 'Tạo user Admin', '1', 'Tài khoản Admin');
-INSERT INTO `permission` VALUES ('4', 'user_edit', 'Sửa user Admin', '1', 'Tài khoản Admin');
-INSERT INTO `permission` VALUES ('5', 'user_change_pass', 'Thay đổi user Admin', '1', 'Tài khoản Admin');
-INSERT INTO `permission` VALUES ('6', 'user_remove', 'Xóa user Admin', '1', 'Tài khoản Admin');
-INSERT INTO `permission` VALUES ('7', 'group_user_view', 'Xem nhóm quyền', '1', 'Nhóm quyền');
-INSERT INTO `permission` VALUES ('8', 'group_user_create', 'Tạo nhóm quyền', '1', 'Nhóm quyền');
-INSERT INTO `permission` VALUES ('9', 'group_user_edit', 'Sửa nhóm quyền', '1', 'Nhóm quyền');
-INSERT INTO `permission` VALUES ('10', 'permission_full', 'Full tạo quyền', '1', 'Tạo quyền');
-INSERT INTO `permission` VALUES ('11', 'permission_create', 'Tạo tạo quyền', '1', 'Tạo quyền');
-INSERT INTO `permission` VALUES ('12', 'permission_edit', 'Sửa tạo quyền', '1', 'Tạo quyền');
-INSERT INTO `permission` VALUES ('13', 'banner_full', 'Full quảng cáo', '1', 'Quyền quảng cáo');
-INSERT INTO `permission` VALUES ('14', 'banner_view', 'Xem quảng cáo', '1', 'Quyền quảng cáo');
-INSERT INTO `permission` VALUES ('15', 'banner_delete', 'Xóa quảng cáo', '1', 'Quyền quảng cáo');
-INSERT INTO `permission` VALUES ('16', 'banner_create', 'Tạo quảng cáo', '1', 'Quyền quảng cáo');
-INSERT INTO `permission` VALUES ('17', 'banner_edit', 'Sửa quảng cáo', '1', 'Quyền quảng cáo');
-INSERT INTO `permission` VALUES ('18', 'category_full', 'Full danh mục', '1', 'Quyền danh mục');
-INSERT INTO `permission` VALUES ('19', 'category_view', 'Xem danh mục', '1', 'Quyền danh mục');
-INSERT INTO `permission` VALUES ('20', 'category_delete', 'Xóa danh mục', '1', 'Quyền danh mục');
-INSERT INTO `permission` VALUES ('21', 'category_create', 'Tạo danh mục', '1', 'Quyền danh mục');
-INSERT INTO `permission` VALUES ('22', 'category_edit', 'Sửa danh mục', '1', 'Quyền danh mục');
-INSERT INTO `permission` VALUES ('23', 'items_full', 'Full tin rao', '1', 'Quyền tin rao');
-INSERT INTO `permission` VALUES ('24', 'items_view', 'Xem tin rao', '1', 'Quyền tin rao');
-INSERT INTO `permission` VALUES ('25', 'items_delete', 'Xóa tin rao', '1', 'Quyền tin rao');
-INSERT INTO `permission` VALUES ('26', 'items_create', 'Tạo tin rao', '1', 'Quyền tin rao');
-INSERT INTO `permission` VALUES ('27', 'items_edit', 'Sửa tin rao', '1', 'Quyền tin rao');
-INSERT INTO `permission` VALUES ('28', 'news_full', 'Full tin tức', '1', 'Quyền tin tức');
-INSERT INTO `permission` VALUES ('29', 'news_view', 'Xem tin tức', '1', 'Quyền tin tức');
-INSERT INTO `permission` VALUES ('30', 'news_delete', 'Xóa tin tức', '1', 'Quyền tin tức');
-INSERT INTO `permission` VALUES ('31', 'news_create', 'Tạo tin tức', '1', 'Quyền tin tức');
-INSERT INTO `permission` VALUES ('32', 'news_edit', 'Sửa tin tức', '1', 'Quyền tin tức');
-INSERT INTO `permission` VALUES ('33', 'province_full', 'Full tỉnh thành', '1', 'Quyền tỉnh thành');
-INSERT INTO `permission` VALUES ('34', 'province_view', 'Xem tỉnh thành', '1', 'Quyền tỉnh thành');
-INSERT INTO `permission` VALUES ('35', 'province_delete', 'Xóa tỉnh thành', '1', 'Quyền tỉnh thành');
-INSERT INTO `permission` VALUES ('36', 'province_create', 'Tạo tỉnh thành', '1', 'Quyền tỉnh thành');
-INSERT INTO `permission` VALUES ('37', 'province_edit', 'Sửa tỉnh thành', '1', 'Quyền tỉnh thành');
-INSERT INTO `permission` VALUES ('38', 'user_customer_full', 'Full khách hàng', '1', 'Quyền khách hàng');
-INSERT INTO `permission` VALUES ('39', 'user_customer_view', 'Xem khách hàng', '1', 'Quyền khách hàng');
-INSERT INTO `permission` VALUES ('40', 'user_customer_delete', 'Xóa khách hàng', '1', 'Quyền khách hàng');
-INSERT INTO `permission` VALUES ('41', 'user_customer_create', 'Tạo khách hàng', '1', 'Quyền khách hàng');
-INSERT INTO `permission` VALUES ('42', 'user_customer_edit', 'Sửa khách hàng', '1', 'Quyền khách hàng');
-INSERT INTO `permission` VALUES ('43', 'toolsCommon_full', 'Full quyền', '1', 'Full quyền Share link');
-INSERT INTO `permission` VALUES ('44', 'libraryImage_full', 'Full thư viện ảnh', '1', 'Quyền thư viện ảnh');
-INSERT INTO `permission` VALUES ('45', 'libraryImage_view', 'Xem thư viện ảnh', '1', 'Quyền thư viện ảnh');
-INSERT INTO `permission` VALUES ('46', 'libraryImage_delete', 'Xóa thư viện ảnh', '1', 'Quyền thư viện ảnh');
-INSERT INTO `permission` VALUES ('47', 'libraryImage_create', 'Tạo thư viện ảnh', '1', 'Quyền thư viện ảnh');
-INSERT INTO `permission` VALUES ('48', 'libraryImage_edit', 'Sửa thư viện ảnh', '1', 'Quyền thư viện ảnh');
-INSERT INTO `permission` VALUES ('49', 'video_full', 'Full video', '1', 'Quyền video');
-INSERT INTO `permission` VALUES ('50', 'video_view', 'Xem video', '1', 'Quyền video');
-INSERT INTO `permission` VALUES ('51', 'video_delete', 'Xóa video', '1', 'Quyền video');
-INSERT INTO `permission` VALUES ('52', 'video_create', 'Tạo video', '1', 'Quyền video');
-INSERT INTO `permission` VALUES ('53', 'video_edit', 'Sửa video', '1', 'Quyền video');
-INSERT INTO `permission` VALUES ('54', 'infor_full', 'Full thông tin site', '1', 'Quyền thông tin site');
-INSERT INTO `permission` VALUES ('55', 'infor_view', 'Xem thông tin site', '1', 'Quyền thông tin site');
-INSERT INTO `permission` VALUES ('56', 'infor_delete', 'Xóa thông tin site', '1', 'Quyền thông tin site');
-INSERT INTO `permission` VALUES ('57', 'infor_create', 'Tạo thông tin site', '1', 'Quyền thông tin site');
-INSERT INTO `permission` VALUES ('58', 'infor_edit', 'Sửa thông tin site', '1', 'Quyền thông tin site');
-INSERT INTO `permission` VALUES ('59', 'user_view', 'Xem danh sách user Admin', '1', 'Tài khoản Admin');
+INSERT INTO `cms_permission` VALUES ('1', 'is_boss', 'Root', '1', 'Root');
+INSERT INTO `cms_permission` VALUES ('2', 'supper_admin', 'Admin', '1', 'Admin');
+INSERT INTO `cms_permission` VALUES ('3', 'user_create', 'Tạo user Admin', '1', 'Tài khoản Admin');
+INSERT INTO `cms_permission` VALUES ('4', 'user_edit', 'Sửa user Admin', '1', 'Tài khoản Admin');
+INSERT INTO `cms_permission` VALUES ('5', 'user_change_pass', 'Thay đổi user Admin', '1', 'Tài khoản Admin');
+INSERT INTO `cms_permission` VALUES ('6', 'user_remove', 'Xóa user Admin', '1', 'Tài khoản Admin');
+INSERT INTO `cms_permission` VALUES ('7', 'group_user_view', 'Xem nhóm quyền', '1', 'Nhóm quyền');
+INSERT INTO `cms_permission` VALUES ('8', 'group_user_create', 'Tạo nhóm quyền', '1', 'Nhóm quyền');
+INSERT INTO `cms_permission` VALUES ('9', 'group_user_edit', 'Sửa nhóm quyền', '1', 'Nhóm quyền');
+INSERT INTO `cms_permission` VALUES ('10', 'permission_full', 'Full tạo quyền', '1', 'Tạo quyền');
+INSERT INTO `cms_permission` VALUES ('11', 'permission_create', 'Tạo tạo quyền', '1', 'Tạo quyền');
+INSERT INTO `cms_permission` VALUES ('12', 'permission_edit', 'Sửa tạo quyền', '1', 'Tạo quyền');
+INSERT INTO `cms_permission` VALUES ('13', 'banner_full', 'Full quảng cáo', '1', 'Quyền quảng cáo');
+INSERT INTO `cms_permission` VALUES ('14', 'banner_view', 'Xem quảng cáo', '1', 'Quyền quảng cáo');
+INSERT INTO `cms_permission` VALUES ('15', 'banner_delete', 'Xóa quảng cáo', '1', 'Quyền quảng cáo');
+INSERT INTO `cms_permission` VALUES ('16', 'banner_create', 'Tạo quảng cáo', '1', 'Quyền quảng cáo');
+INSERT INTO `cms_permission` VALUES ('17', 'banner_edit', 'Sửa quảng cáo', '1', 'Quyền quảng cáo');
+INSERT INTO `cms_permission` VALUES ('18', 'category_full', 'Full danh mục', '1', 'Quyền danh mục');
+INSERT INTO `cms_permission` VALUES ('19', 'category_view', 'Xem danh mục', '1', 'Quyền danh mục');
+INSERT INTO `cms_permission` VALUES ('20', 'category_delete', 'Xóa danh mục', '1', 'Quyền danh mục');
+INSERT INTO `cms_permission` VALUES ('21', 'category_create', 'Tạo danh mục', '1', 'Quyền danh mục');
+INSERT INTO `cms_permission` VALUES ('22', 'category_edit', 'Sửa danh mục', '1', 'Quyền danh mục');
+INSERT INTO `cms_permission` VALUES ('23', 'items_full', 'Full tin rao', '1', 'Quyền tin rao');
+INSERT INTO `cms_permission` VALUES ('24', 'items_view', 'Xem tin rao', '1', 'Quyền tin rao');
+INSERT INTO `cms_permission` VALUES ('25', 'items_delete', 'Xóa tin rao', '1', 'Quyền tin rao');
+INSERT INTO `cms_permission` VALUES ('26', 'items_create', 'Tạo tin rao', '1', 'Quyền tin rao');
+INSERT INTO `cms_permission` VALUES ('27', 'items_edit', 'Sửa tin rao', '1', 'Quyền tin rao');
+INSERT INTO `cms_permission` VALUES ('28', 'news_full', 'Full tin tức', '1', 'Quyền tin tức');
+INSERT INTO `cms_permission` VALUES ('29', 'news_view', 'Xem tin tức', '1', 'Quyền tin tức');
+INSERT INTO `cms_permission` VALUES ('30', 'news_delete', 'Xóa tin tức', '1', 'Quyền tin tức');
+INSERT INTO `cms_permission` VALUES ('31', 'news_create', 'Tạo tin tức', '1', 'Quyền tin tức');
+INSERT INTO `cms_permission` VALUES ('32', 'news_edit', 'Sửa tin tức', '1', 'Quyền tin tức');
+INSERT INTO `cms_permission` VALUES ('33', 'province_full', 'Full tỉnh thành', '1', 'Quyền tỉnh thành');
+INSERT INTO `cms_permission` VALUES ('34', 'province_view', 'Xem tỉnh thành', '1', 'Quyền tỉnh thành');
+INSERT INTO `cms_permission` VALUES ('35', 'province_delete', 'Xóa tỉnh thành', '1', 'Quyền tỉnh thành');
+INSERT INTO `cms_permission` VALUES ('36', 'province_create', 'Tạo tỉnh thành', '1', 'Quyền tỉnh thành');
+INSERT INTO `cms_permission` VALUES ('37', 'province_edit', 'Sửa tỉnh thành', '1', 'Quyền tỉnh thành');
+INSERT INTO `cms_permission` VALUES ('38', 'user_customer_full', 'Full khách hàng', '1', 'Quyền khách hàng');
+INSERT INTO `cms_permission` VALUES ('39', 'user_customer_view', 'Xem khách hàng', '1', 'Quyền khách hàng');
+INSERT INTO `cms_permission` VALUES ('40', 'user_customer_delete', 'Xóa khách hàng', '1', 'Quyền khách hàng');
+INSERT INTO `cms_permission` VALUES ('41', 'user_customer_create', 'Tạo khách hàng', '1', 'Quyền khách hàng');
+INSERT INTO `cms_permission` VALUES ('42', 'user_customer_edit', 'Sửa khách hàng', '1', 'Quyền khách hàng');
+INSERT INTO `cms_permission` VALUES ('43', 'toolsCommon_full', 'Full quyền', '1', 'Full quyền Share link');
+INSERT INTO `cms_permission` VALUES ('44', 'libraryImage_full', 'Full thư viện ảnh', '1', 'Quyền thư viện ảnh');
+INSERT INTO `cms_permission` VALUES ('45', 'libraryImage_view', 'Xem thư viện ảnh', '1', 'Quyền thư viện ảnh');
+INSERT INTO `cms_permission` VALUES ('46', 'libraryImage_delete', 'Xóa thư viện ảnh', '1', 'Quyền thư viện ảnh');
+INSERT INTO `cms_permission` VALUES ('47', 'libraryImage_create', 'Tạo thư viện ảnh', '1', 'Quyền thư viện ảnh');
+INSERT INTO `cms_permission` VALUES ('48', 'libraryImage_edit', 'Sửa thư viện ảnh', '1', 'Quyền thư viện ảnh');
+INSERT INTO `cms_permission` VALUES ('49', 'video_full', 'Full video', '1', 'Quyền video');
+INSERT INTO `cms_permission` VALUES ('50', 'video_view', 'Xem video', '1', 'Quyền video');
+INSERT INTO `cms_permission` VALUES ('51', 'video_delete', 'Xóa video', '1', 'Quyền video');
+INSERT INTO `cms_permission` VALUES ('52', 'video_create', 'Tạo video', '1', 'Quyền video');
+INSERT INTO `cms_permission` VALUES ('53', 'video_edit', 'Sửa video', '1', 'Quyền video');
+INSERT INTO `cms_permission` VALUES ('54', 'infor_full', 'Full thông tin site', '1', 'Quyền thông tin site');
+INSERT INTO `cms_permission` VALUES ('55', 'infor_view', 'Xem thông tin site', '1', 'Quyền thông tin site');
+INSERT INTO `cms_permission` VALUES ('56', 'infor_delete', 'Xóa thông tin site', '1', 'Quyền thông tin site');
+INSERT INTO `cms_permission` VALUES ('57', 'infor_create', 'Tạo thông tin site', '1', 'Quyền thông tin site');
+INSERT INTO `cms_permission` VALUES ('58', 'infor_edit', 'Sửa thông tin site', '1', 'Quyền thông tin site');
+INSERT INTO `cms_permission` VALUES ('59', 'user_view', 'Xem danh sách user Admin', '1', 'Tài khoản Admin');
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for cms_user
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `cms_user`;
+CREATE TABLE `cms_user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) NOT NULL,
   `user_password` varchar(255) NOT NULL,
   `user_full_name` varchar(255) NOT NULL,
   `user_email` varchar(255) DEFAULT NULL,
   `user_phone` varchar(11) DEFAULT NULL,
+  `user_service` varchar(255) DEFAULT NULL COMMENT 'Chức vụ',
+  `user_time_work_start` int(11) DEFAULT '0' COMMENT 'Thời gian bắt đầu làm việc',
+  `user_time_work_end` int(11) DEFAULT NULL COMMENT 'Thời gian nghỉ',
+  `user_group_depart` varchar(200) DEFAULT NULL COMMENT 'Thuộc nhóm khoa: 1,2,3..',
   `user_status` int(2) NOT NULL DEFAULT '1' COMMENT '-1: xÃ³a , 1: active',
   `user_group` varchar(255) DEFAULT NULL,
   `user_last_login` int(11) DEFAULT NULL,
@@ -143,17 +132,34 @@ CREATE TABLE `user` (
   `user_create_name` varchar(255) DEFAULT NULL,
   `user_edit_id` int(11) DEFAULT NULL,
   `user_edit_name` varchar(255) DEFAULT NULL,
-  `user_created` int(11) DEFAULT NULL,
-  `user_updated` int(11) DEFAULT NULL,
+  `user_time_created` int(11) DEFAULT NULL,
+  `user_time_updated` int(11) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of user
+-- Records of cms_user
 -- ----------------------------
-INSERT INTO `user` VALUES ('2', 'admin', 'eef828faf0754495136af05c051766cb', 'Root', '', null, '1', '1', '1485140686', '::1', null, null, null, null, null, null);
-INSERT INTO `user` VALUES ('19', 'tech_code', '7eb3b9aba1960c22aa9bc8d1f27ebfb9', 'Tech code 3555', '', '', '1', '2', '1481772767', '::1', null, null, '2', 'admin', null, '1481772561');
-INSERT INTO `user` VALUES ('20', 'svquynhtm', 'a1f54bbcea29cf49935e0a5ead5a3dfa', 'Trương Mạnh Quỳnh', 'manhquynh1984@gmail.com', '0938413368', '1', '2', '1482826054', '::1', '2', 'admin', '2', 'admin', '1482823830', '1482824272');
+INSERT INTO `cms_user` VALUES ('2', 'admin', 'eef828faf0754495136af05c051766cb', 'Root', '', null, null, '0', null, null, '1', '1', '1485164027', '::1', null, null, null, null, null, null);
+INSERT INTO `cms_user` VALUES ('19', 'tech_code', '7eb3b9aba1960c22aa9bc8d1f27ebfb9', 'Tech code 3555', '', '', '', '0', '0', null, '1', '2', '1481772767', '::1', null, null, '2', 'admin', null, '1481772561');
+INSERT INTO `cms_user` VALUES ('20', 'svquynhtm', 'fa268d7af7410dbf1b860075e9074889', 'Trương Mạnh Quỳnh', 'manhquynh1984@gmail.com', '0938413368', 'Cộng tác viên', '1483203600', '1484240400', null, '1', '2', '1482826054', '::1', '2', 'admin', '2', 'admin', '1482823830', '1482824272');
+
+-- ----------------------------
+-- Table structure for cms_user_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `cms_user_permission`;
+CREATE TABLE `cms_user_permission` (
+  `user_permission_id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_user_id` int(11) NOT NULL COMMENT 'id nhÃ³m',
+  `permission_id` int(11) NOT NULL COMMENT 'id quyÃ¨n',
+  PRIMARY KEY (`user_permission_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of cms_user_permission
+-- ----------------------------
+INSERT INTO `cms_user_permission` VALUES ('1', '1', '1');
+INSERT INTO `cms_user_permission` VALUES ('2', '2', '43');
 
 -- ----------------------------
 -- Table structure for w_banner
