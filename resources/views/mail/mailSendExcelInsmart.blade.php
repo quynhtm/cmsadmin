9f1d99e863911764101b67e7d5aa7c5627d744c6
quynhtm <manhquynh1984@gmail.com>
2021-12-09 17:01:23 +0700

    ver 8

--- resources/views/mail/mailSendExcelInsmart.blade.php
+++
@@ -1,5 +0,0 @@
-<br/> HDI gửi Insmart Danh sách cấp đơn ngày <?php echo date('d/m/Y'); ?>
-<br/> Danh sách chi tiết theo File đính kèm..
-<br/><br/>
-<br/>Trân trọng ./.
-<br/>Không trả lời mail tự động này. Xin cảm ơn.
