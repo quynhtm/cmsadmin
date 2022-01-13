661e69d8ac21075dcd67f0b38ccb8237f3ba298b
quynhtm <manhquynh1984@gmail.com>
2022-01-13 18:42:09 +0700

    ver 8

--- resources/views/Shop/Orders/component/_detailFormItem.blade.php
+++ resources/views/Shop/Orders/component/_detailFormItem.blade.php
@@ -100,8 +100,8 @@
                     <thead class="thin-border-bottom">
                     <tr class="table-background-header">
                         <th width="5%" class="text-center">STT</th>
-                        <th width="10%" class="text-center">ID Sản phẩm</th>
-                        <th width="50%" class="text-center">Sản phẩm</th>
+                        <th width="10%" class="text-center">ID SP</th>
+                        <th width="50%" class="text-left">Sản phẩm</th>
                         <th width="15%" class="text-right">Giá bán</th>
                         <th width="10%" class="text-center">Số lượng</th>
                         <th width="20%" class="text-right">Tổng tiền</th>
@@ -109,36 +109,12 @@
                     </thead>
                     <tbody>
                     <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td>61</td>
-                        <td>2011/04/25</td>
-                        <td>$320,800</td>
-                    </tr>
-                    <tr>
-                        <td>Garrett Winters</td>
-                        <td>Accountant</td>
-                        <td>Tokyo</td>
-                        <td>63</td>
-                        <td>2011/07/25</td>
-                        <td>$170,750</td>
-                    </tr>
-                    <tr>
-                        <td>Ashton Cox</td>
-                        <td>Junior Technical Author</td>
-                        <td>San Francisco</td>
-                        <td>66</td>
-                        <td>2009/01/12</td>
-                        <td>$86,000</td>
-                    </tr>
-                    <tr>
-                        <td>Cedric Kelly</td>
-                        <td>Senior Javascript Developer</td>
-                        <td>Edinburgh</td>
-                        <td>22</td>
-                        <td>2012/03/29</td>
-                        <td>$433,060</td>
+                        <td class="text-center">1</td>
+                        <td class="text-center">111</td>
+                        <td class="text-left">tên sản phẩm</td>
+                        <td class="text-right">10.000</td>
+                        <td class="text-center"><input type="number"></td>
+                        <td class="text-center">10.000</td>
                     </tr>
                 </table>
             </div>
